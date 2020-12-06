62a175b149d4b30b63e1f3b55e003c5c900e2fc8
Devendra <devendra.chaudhari@bluetie.co.in>
2020-12-06 14:09:06 +0530

    Activity created

--- app/src/main/java/com/example/recyclermvvmapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/recyclermvvmapp/ui/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.example.recyclermvvmapp
+package com.example.recyclermvvmapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.recyclermvvmapp.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
     }
 }
